2932908260d7ec53778e166bc0c7a59e352b2612
Ntiendat-2k3 <nguyentiendatg2003@gmail.com>
2025-04-10 22:13:14 +0700

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link'
- 
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h2 className="text-2xl font-bold mb-4">404 - Page Not Found</h2>
+      <p className="mb-4">Could not find requested resource</p>
+      <Link 
+        href="/" 
+        className="text-blue-500 hover:underline"
+      >
+        Return Home
+      </Link>
     </div>
-    
   )
 }
